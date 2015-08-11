2c095207c0d803c3d83f203d373663839b8c165a
Vikash <vikash@dataemo.com>
2015-08-11 16:53:09 +0530

    added adin model controls

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tweet
+
+class TweetAdmin(admin.ModelAdmin):
+    fields = ['tweet_text']
+
+admin.site.register(Tweet, TweetAdmin)
--- tweets/models.py
+++ tweets/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Tweet(models.Model):
     tweet_text = models.CharField(max_length=140)
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.tweet_text
